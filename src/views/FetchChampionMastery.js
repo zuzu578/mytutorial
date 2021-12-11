71598fcaa856e31197e48d13ce12fbf29306b89b
zuzu <dlwnghks6821@naver.com>
2021-12-11 19:40:50 +0900

    promise return (axios) 후 , object를 외부에서 가져오게 하기위해서 callback

--- src/views/FetchChampionMastery.js
+++ src/views/FetchChampionMastery.js
@@ -8,18 +8,19 @@ import axios from 'axios';
  * @param {*} promise 
  * @returns 
  */
-const FetchChampionMastery = async (encryptedSummonerId,e,promise) =>{
+const FetchChampionMastery = async (encryptedSummonerId, e, promise) => {
+    // axios => promise 기반  === promise object return 
+    try {
 
-let fetchData = await axios.get('https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/'+encryptedSummonerId+'?api_key='+api_key+'');
-    return new Promise((resolve, reject)=>{
-        resolve(fetchData.data);
-    })
-    
-    .then((res)=>{
-        return promise(res);
-    })
-    
-    
+        let fetchData = await axios.get('https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/' + encryptedSummonerId + '?api_key=' + api_key + '');
+        //axios 통신후 , promise 객체를 return 한 object를 변수에 담고 , 콜백으로 인자값을 전달한다.
+        return promise(fetchData.data);
+
+    } catch (error) {
+
+        console.log(error);
+    }
+
 }
 
 export default FetchChampionMastery;
