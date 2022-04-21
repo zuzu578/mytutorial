d4e8fb1672e769c81f61e89a0804c4350a2319fa
zuzu <dlwnghks6821@naver.com>
2022-04-21 19:27:59 +0900

    coc

--- src/component/spectator.js
+++ src/component/spectator.js
@@ -8,14 +8,20 @@ const Spectator = (props) =>{
     const [isGamein , setIsGameIn] = useState(true);
 
 console.log('propsid=>',props.props);
+useState(()=>{
+
+},[])
 const getData = () => {
     
     getSpectatorData(props.props)
     .then((res)=>{
         console.log('res!!!',res.data);
-        setSpectator(res.data);
+        return res.data;
     }).catch((error)=>{
         setIsGameIn(false);
+    })
+    .then((res)=>{
+        setSpectator(res.data);
     })
     setclicked(true);
 }
@@ -28,7 +34,18 @@ const RenderingSpectatorData = () => {
                  {isGamein === false ? 
                  <div className="notPlaying"> 
                      <p> 해당 소환사는 게임중이아닙니다.</p>
-                 </div> : ''}
+                 </div> : 
+                //  spectator.bannedChampions.map((item)=>{
+                //      return(
+                //          <>
+                //          <p>밴한 챔피언 목록</p>
+                //          <div>
+                //              {item.championId}
+                //           </div>
+                //          </>
+                //      )
+                //  })
+                 ''}
             
            
              
